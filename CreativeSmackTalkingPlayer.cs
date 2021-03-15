fe0e1fb5de9eb758d54ba77fb034db75103bd874
Tim Curtis <timcurtis97@yahoo.com>
2021-03-15 14:50:16 -0500

    finished smack talk player

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,24 @@
+using System.Collections.Generic;
+using System;
+
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
+    public class CreativeSmackTalkingPlayer : Player
     {
+        public List<string> Talk { get; } = new List<string>()
+        {
+            "You are the reason they put instructions on shampoo.",
+            "You are bad and you should feel bad",
+            "Garbage",
+            "You're an inspiration for birth control."
+        };
 
+        public override void Play(Player other)
+        {
+            int x = new Random().Next(6) + 1;
+            Console.WriteLine(Talk[x]);
+            base.Play(other);
+        }
     }
 }
