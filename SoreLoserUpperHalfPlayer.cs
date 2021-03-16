f3b35fec3e6ff990c4fff59f7f919a7335535a5a
Tim Curtis <timcurtis97@yahoo.com>
2021-03-16 11:48:12 -0500

    finished project

--- SoreLoserUpperHalfPlayer.cs
+++ SoreLoserUpperHalfPlayer.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace ShootingDice
 {
-    // TODO: Complete this class
+
 
     // A Player who always rolls in the upper half of their possible role and
     //  who throws an exception when they lose to the other player
